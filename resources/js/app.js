a4e6c207fdc4e4937218dcd35ecf4133d9f568cf
Kazin <xkazinx@gmail.com>
2019-07-03 16:16:27 -0300

    Template createad with: - Laravel - Vue - Vue Router - Element UI - Debugbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,24 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+/**
+ * Element UI.
+ */
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
+Vue.use(ElementUI, { locale })
+
+/**
+ * Vue Router.
+ */
+import VueRouter from 'vue-router';
+import routes from '@/routes/index.js';
+Vue.use(VueRouter);
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,14 +33,16 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('vue-app', require('./App.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
 const app = new Vue({
     el: '#app',
+    router: new VueRouter({
+        routes
+    }),
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,18 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+require('dotenv').config();
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(
+                __dirname,
+                "resources/js"
+            )
+        }
+    }
+ });
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
