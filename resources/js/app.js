cf74a235d8ecde8de6ef5e03e7ad0f3397676cb7
Gabriel Prieto Cassano <gabrielprietocassano@gmail.com>
2019-07-04 21:55:40 -0300

    Sharing env variables with front-end and some code comments

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,7 +12,7 @@ window.Vue = require('vue');
  */
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/en';
+let locale = require(`element-ui/lib/locale/lang/${process.env.MIX_APP_LANG}`).default;
 Vue.use(ElementUI, { locale })
 
 /**
@@ -26,22 +26,27 @@ Vue.use(VueRouter);
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('vue-app', require('./App.vue').default);
+/**
+ * The process.env object contains all the variables that starts with MIX_ in the .env file.
+ * If one of those variables is changed, it will be required to re-compile the assets.
+ * If the assets are beeing compiled using the watch|hot process, it's required to restart the process too.
+ */
+if (process.env.MIX_APP_DEBUG) console.log("[app.js] App is in DEBUG mode!");
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * Next, we will create a fresh Vue application instance and attach it to the page.
+ * Also, we define the root component of the application and set it as the template of the application.
  */
-const app = new Vue({
+Vue.component('vue-app', require('./App.vue').default);
+
+new Vue({
     el: '#app',
+    template: '<vue-app/>',
     router: new VueRouter({
         routes
     }),
--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -1,3 +1,11 @@
-export default [
-    { path: '/example-route', component: require('@/components/ExampleComponent.vue').default }
-];
+/**
+ * Here, we define all the front-end routes, setting the components that will be
+ * rendered in each of them. This routes are thinked to be used with VueRouter,
+ * so we follow it's conventions.
+ */
+const ExampleRoute = {
+    path: '/example-route',
+    component: require('@/components/ExampleComponent.vue').default
+};
+
+export default [ExampleRoute];
